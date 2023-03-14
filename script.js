782cebe0a67fc5bdada532ad6681252c7b0ceed9
Anderson Reis <andersonreis1234555@gmail.com>
2023-03-14 02:50:41 -0300

    terminado

--- script.js
+++ script.js
@@ -8,25 +8,22 @@ import {
 
 
 
-const chatList = document.createElement('div')
-chatList.id = "chatListId"
-const criarContatos = (contato) => {
 
+
+const createContacts = (contato, indice) => {
+    const chatList = document.createElement('div')
     chatList.classList.add('chatlist')
-
-
 
     const blockActive = document.createElement('div')
     blockActive.classList.add("block")
-
+    blockActive.setAttribute('name', contato.name)
 
     const imgbx = document.createElement('div')
     imgbx.classList.add('imgbx')
 
-    const img = document.createElement('img')
-    img.classList.add('imageReform')
-    img.src = `./${contato.image}`
-
+    const imgUser = document.createElement('img')
+    imgUser.classList.add('imageReform')
+    imgUser.src = `./${contato.image}`
 
     const details = document.createElement('div')
     details.classList.add('details')
@@ -34,10 +31,8 @@ const criarContatos = (contato) => {
     const listhead = document.createElement('div')
     listhead.classList.add('listhead')
 
-        const h4 = document.createElement('h4')
-        h4.textContent = contato.name
-   
-    
+    const name = document.createElement('h4')
+    name.textContent = contato.name
 
     const time = document.createElement('p')
     time.classList.add('time')
@@ -48,169 +43,137 @@ const criarContatos = (contato) => {
     for (let contador = 0; contador < contato.messages.length; contador++) {
         time.textContent = contato.messages[contador].time
 
-        group.textContent = contato.messages[contador].content.slice(0, 25) + "..."
+        group.textContent = contato.messages[contador].content.slice(0, 20) + "..."
+
+
     }
-
-
-
-
 
     const message = document.createElement('div')
     message.classList.add('message')
 
-
-
-
-
     chatList.append(blockActive)
     blockActive.append(imgbx, details)
-    imgbx.append(img)
+    imgbx.append(imgUser)
     details.append(listhead, message)
-    listhead.append(h4, time)
+    listhead.append(name, time)
     message.append(group)
+
+
+    blockActive.addEventListener('click', (event) => {
+        const container = document.getElementById('box-right')
+        container.replaceChildren(createMessage(indice))
+    })
+   
 
     return chatList
 
-
-
 }
-
-
-// function AdicionarParteDireitaPerfil(contato) {
-
-//     if(getNome == )
-//     // parte de cima do perfil
-//     const imgPerfil = document.createElement('div')
-//     imgPerfil.classList.add('img-perfil')
-
-//     const userimg = document.createElement('div')
-//     imgPerfil.classList.add('userimg')
-//     imgWhatsapp.src = `./${contato.image}`
-//     const imgWhatsapp = document.createElement('img')
-   
-
-//     const nome = document.createElement('h4')
-//     nome.textContent = contato.name
-
-//     imgPerfil.append(userimg, nome)
-//     userimg.append(imgWhatsapp)
-
-//     return imgPerfil
-
-
-// }
-
-// // parte das conversas do perfil
-// function adicionarParteDireitaChat(contato) {
-//     const chat = document.createElement('div')
-//     chat.classList.add('chat')
-
-//     const youMessage = document.createElement('div')
-//     youMessage.classList.add('message', 'you-message')
-
-//     const p1 = document.createElement('p')
-//     const br = document.createElement('br')
-
-//     const span = document.createElement('span')
-//     for (let contador = 0; contador < contato.messages.length; contator++) {
-//         p1.textContent = contato.messages[contador]
-//         span.textContent = contato.messages[contador].time
-//     }
-//     contato.message.forEach((contador)=>{
-//         p1.textContent = contador.sender
-//         span.textContent = contador.time
-//     })
-
-
-
-//     chat.append(youMessage)
-//     p1.append(br, span)
-
-//     return chat
-
-
-// }
-
-
-
-
-
-// const carregarLadoDireito1 = () => {
-//     const perfil = document.getElementById('addLadoDireito1')
-
-
-
-//     const perfilScript = contatos.map(AdicionarParteDireitaPerfil)
-
-//     return perfil.replaceChildren(...perfilScript)
-
-
-
-
-// }
-
-// const carregarLadoDireito2 = () => {
-
-//     const chat = document.getElementById('addLadoDireito2')
-
-
-
-//     const chatScript = contatos.map(adicionarParteDireitaChat)
-
-//     return chat.replaceChildren(...chatScript)
-
-
-
-// }
-
-
-
-const carregarContatos = () => {
-
-    const container = document.getElementById('container2')
-    const contatosScript = contatos.map(criarContatos)
-    container.replaceChildren(...contatosScript)
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-chatList.addEventListener('click', () => {
-
-    // carregarLadoDireito1()
-    // carregarLadoDireito2()
-    const boxLeft = document.getElementById('box-left')
-    const boxRigth = document.getElementById('box-right')
-    const arrowLeft = document.getElementById('arrow-left')
-
-    boxLeft.classList.add('activeDesaparecer')
-    boxRigth.classList.add('activeAparacer')
-
-
-
-        arrowLeft.addEventListener('click', () => {
-   
-            boxLeft.classList.remove('activeDesaparecer')
-            boxRigth.classList.remove('activeAparacer')
-    
-
-        })
-
-
-  
+const arrowBack = document.createElement('div')
+const createMessage = (indice) => {
+
+    const boxRight = document.createElement('div')
+    boxRight.classList.add('box-right', 'box-right::before')
+    boxRight.id = 'box-right'
+
+    const header = document.createElement('div')
+    header.classList.add('header')
+    const contactUserImg = document.createElement('div')
+    const userPerfli = document.createElement('div')
+    userPerfli.classList.add('userimg')
+    contactUserImg.classList.add('img-perfil')
+    const contactImg = document.createElement('img')
+    contactImg.classList.add('cover')
+    contactImg.src = `./${contatos[indice].image}`
+    const contactName = document.createElement('h4')
+    contactName.textContent = contatos[indice].name
+    const space = document.createElement('br')
+    const contactDescription = document.createElement('span')
+    contactDescription.textContent = contatos[indice].description
+
+    arrowBack.classList.add('arrow-left::before')
+
+    const contactUl = document.createElement('ul')
+    contactUl.classList.add('icons')
+    const li1 = document.createElement('li')
+    const li2 = document.createElement('li')
+
+    const ionIconSearch = document.createElement('ion-icon')
+    ionIconSearch.setAttribute('name', 'search-outline')
+    const ionIconEllipsis = document.createElement('ion-icon')
+    ionIconEllipsis.setAttribute('name', 'ellipsis-vertical')
+
+
+    const chat = document.createElement('div')
+    chat.classList.add('chat')
+    contatos[indice].messages.forEach(function (mensagem) {
+        if (mensagem.sender == "me") {
+            const myMessage = document.createElement('div')
+            myMessage.classList.add("message", "you-message")
+
+            const userMessage = document.createElement('p')
+            userMessage.textContent = mensagem.content
+
+            const timeStamp = document.createElement('span')
+            timeStamp.textContent = mensagem.time
+
+            myMessage.append(userMessage)
+            userMessage.append(timeStamp)
+            chat.appendChild(myMessage)
+        } else {
+            const friendMessage = document.createElement('div')
+            friendMessage.classList.add("message", "friend-message")
+
+            const contactMessage = document.createElement('p')
+            contactMessage.textContent = mensagem.content
+
+            const friendTimeStamp = document.createElement('span')
+            friendTimeStamp.textContent = mensagem.time
+
+            friendMessage.append(contactMessage)
+            contactMessage.append(friendTimeStamp)
+            chat.appendChild(friendMessage)
+        }
     })
 
+    const chatInput = document.createElement('div')
+    chatInput.classList.add('chatbox-input')
+    const ionIconHappy = document.createElement('ion-icon')
+    ionIconHappy.setAttribute('name', 'happy-outline')
 
-carregarContatos()
+    const ionIconattach = document.createElement('ion-icon')
+    ionIconattach.setAttribute('name', 'attach-outline')
+    const inputMessage = document.createElement('input')
+    inputMessage.setAttribute('type', 'text')
+    inputMessage.setAttribute('placeholder', 'Envie sua Mensagem')
+    const ionIconmic = document.createElement('ion-icon')
+    ionIconmic.setAttribute('name', 'mic-outline')
+
+    boxRight.append(header, chat, chatInput)
+    header.append(contactUserImg, contactUl)
+    contactName.append(space)
+    space.append(contactDescription)
+    contactUserImg.append(arrowBack, userPerfli, contactName)
+    userPerfli.append(contactImg)
+    contactUl.append(li1, li2)
+    li1.append(ionIconSearch)
+    li2.append(ionIconEllipsis)
+
+    chatInput.append(ionIconHappy, ionIconattach, inputMessage, ionIconmic)
+
+
+
+
+    return boxRight
+
+
+}
+
+
+const loadContact = () => {
+    const container = document.getElementById('container2')
+    const SendContact = contatos.map(createContacts)
+    container.replaceChildren(...SendContact)
+}
+
+
+loadContact()
